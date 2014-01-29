a309eeae96d8eccdfbdcaaeeaf75175b1aeee518
David T. Sadler <davidtsadler@googlemail.com>
2014-01-29 21:36:39 +0000

    Add base service class.

---
+++ src/DTS/eBaySDK/FileTransfer/Services/FileTransferBaseService.php
@@ -0,0 +1,10 @@
+<?php
+namespace DTS\eBaySDK\FileTransfer\Services;
+
+class FileTransferBaseService extends \DTS\eBaySDK\Services\BaseService
+{
+    public function __construct($config = [])
+    {
+        parent::__construct($config);
+    }
+}
