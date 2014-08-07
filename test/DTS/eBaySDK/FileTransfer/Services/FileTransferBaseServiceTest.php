5f877ff483e20e93f16fe2cb2a29b01750093c6a
David T. Sadler <davidtsadler@googlemail.com>
2014-08-07 12:10:39 +0100

    Prepare for 0.1.0 release.

--- test/DTS/eBaySDK/FileTransfer/Services/FileTransferBaseServiceTest.php
+++ test/DTS/eBaySDK/FileTransfer/Services/FileTransferBaseServiceTest.php
@@ -1,23 +1,27 @@
 <?php
-
 use DTS\eBaySDK\FileTransfer\Services\FileTransferBaseService;
+use DTS\eBaySDK\Mocks\HttpClient;
 
 class FileTransferBaseServiceTest extends \PHPUnit_Framework_TestCase
 {
-    private $obj;
-
     protected function setUp()
     {
-        $this->obj = new FileTransferBaseService();
+        $this->service1 = new FileTransferBaseService();
+        $this->service2 = new FileTransferBaseService(array());
+        $this->service3 = new FileTransferBaseService(array(), new HttpClient());
     }
 
     public function testCanBeCreated()
     {
-        $this->assertInstanceOf('\DTS\eBaySDK\FileTransfer\Services\FileTransferBaseService', $this->obj);
+        $this->assertInstanceOf('\DTS\eBaySDK\FileTransfer\Services\FileTransferBaseService', $this->service1);
+        $this->assertInstanceOf('\DTS\eBaySDK\FileTransfer\Services\FileTransferBaseService', $this->service2);
+        $this->assertInstanceOf('\DTS\eBaySDK\FileTransfer\Services\FileTransferBaseService', $this->service3);
     }
 
     public function testExtendsBaseService()
     {
-        $this->assertInstanceOf('\DTS\eBaySDK\Services\BaseService', $this->obj);
+        $this->assertInstanceOf('\DTS\eBaySDK\Services\BaseService', $this->service1);
+        $this->assertInstanceOf('\DTS\eBaySDK\Services\BaseService', $this->service2);
+        $this->assertInstanceOf('\DTS\eBaySDK\Services\BaseService', $this->service3);
     }
 }
