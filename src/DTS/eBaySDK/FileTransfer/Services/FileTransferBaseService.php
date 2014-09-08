c9fed3a8194c09a41116939d2524ed7a36a14a52
David T. Sadler <davidtsadler@googlemail.com>
2014-09-08 11:53:41 +0100

    feature: allow request object to set fileAttachment property
    Calling the uploadFile operation requires the fileAttachment property to
    be set correctly in the request object. Without the correct information
    the uploaded file will not be found by the eBay API.
    
    ```php
    $request->attachment('123ABC');
    $request->fileAttachment = new Types\FileAttachment();
    $request->fileAttachment->Data = '<xop:Include xmlns:xop="http://www.w3.org/2004/08/xop/include" href="cid:attachment.bin@devbay.net"/>';
    $request->fileAttachment->Size = 6;
    
    $response = $service->uploadFile($request);
    ```
    
    In order to help users of the SDK the request object will assign the
    correct values to the fileAttachment property when the uploadFile
    operation is called.
    
    ```php
    $request->attachment('123ABC');
    
    $response = $service->uploadFile($request);
    ```

--- src/DTS/eBaySDK/FileTransfer/Services/FileTransferBaseService.php
+++ src/DTS/eBaySDK/FileTransfer/Services/FileTransferBaseService.php
@@ -43,6 +43,44 @@ class FileTransferBaseService extends \DTS\eBaySDK\Services\BaseService
     }
 
     /**
+     * Sends an API request.
+     *
+     * This method overrides the parent so that it can modify
+     * the request object before is handled by the parent class.
+     *
+     * @param string $name The name of the operation.
+     * @param \DTS\eBaySDK\Types\BaseType $request Request object containing the request information.
+     * @param string The name of the PHP class that will be created from the XML response.
+     *
+     * @return mixed A response object created from the XML respose.
+     */
+    protected function callOperation($name, \DTS\eBaySDK\Types\BaseType $request, $responseClass)
+    {
+        /**
+            Modify the request object to add xop:Include element.
+         */
+        if ($name === 'uploadFile' && $request->hasAttachment()) {
+            /**
+                Don't modify a request if the file attachment already exists.
+             */
+            if( !isset($request->fileAttachment)) {
+                $request->fileAttachment = new \DTS\eBaySDK\FileTransfer\Types\FileAttachment();
+            }
+
+            if(!isset($request->fileAttachment->Data)) {
+                $request->fileAttachment->Data = '<xop:Include xmlns:xop="http://www.w3.org/2004/08/xop/include" href="cid:attachment.bin@devbay.net"/>';
+            }
+
+            if(!isset($request->fileAttachment->Size)) {
+                $attachment = $request->attachment();
+                $request->fileAttachment->Size = strlen($attachment['data']);
+            }
+        }
+
+        return parent::callOperation($name, $request, $responseClass);
+    }
+
+    /**
      * Build the needed eBay HTTP headers.
      *
      * @param string $operationName The name of the operation been called.
